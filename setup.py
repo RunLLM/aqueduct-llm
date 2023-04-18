fcd005f8a3d26a6a9d267d08aac139a148ffe0bb
cw75 <cgwu0530@gmail.com>
2023-04-18 00:27:09 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="aqueduct-llm", version="0.0.0", packages=find_packages())
+install_requires = open("requirements.txt").read().strip().split("\n")
+
+setup(
+    name="aqueduct-llm",
+    version="0.0.0",
+    install_requires=install_requires,
+    packages=find_packages(),
+)
