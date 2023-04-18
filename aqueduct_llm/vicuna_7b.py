651d6d62a8e7d171e0d14f4803d5b8921bfc5645
cw75 <cgwu0530@gmail.com>
2023-04-18 23:38:14 +0000

    refactor

--- aqueduct_llm/vicuna_7b.py
+++ aqueduct_llm/vicuna_7b.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -6,15 +7,33 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from fastchat.conversation import get_default_conv_template
 from fastchat.serve.inference import load_model, compute_skip_echo_len
 
+max_gpu_memory_key = 'AQUEDUCT_VICUNA_7B_MAX_GPU_MEMORY'
+default_max_gpu_memory = '13GiB'
+
+class Config:
+    def __init__(self):
+        self.model_path = "/vicuna-7b"
+        self.device = "cuda"
+        self.num_gpus = "1"
+        self.max_gpu_memory = os.environ[max_gpu_memory_key] if max_gpu_memory_key in os.environ else default_max_gpu_memory
+        self.debug = False
+        self.load_8bit = False
+    
+    def describe(self) -> str:
+        print("Running Vicuna 7B with the following config:")
+        attrs = {
+            "model_path": self.model_path,
+            "device": self.device,
+            "num_gpus": self.num_gpus,
+            "max_gpu_memory": self.max_gpu_memory,
+            "debug": self.debug,
+            "load_8bit": self.load_8bit,
+        }
+        print("\n".join([f"{attr}: {value}" for attr, value in attrs.items()]))
 
 @torch.inference_mode()
 def generate(messages):
-    model_path = "/vicuna-7b"
-    device = "cuda"
-    num_gpus = "1"
-    max_gpu_memory = "13GiB"
-    debug = False
-    load_8bit = False
+    config = Config()
 
     if isinstance(messages, str):
         messages = [messages]
@@ -30,12 +49,12 @@ def generate(messages):
     results = []
     
     for message in messages:
-        model, tokenizer = load_model(model_path, device,
-            num_gpus, max_gpu_memory, load_8bit, debug=debug)
+        model, tokenizer = load_model(config.model_path, config.device,
+            config.num_gpus, config.max_gpu_memory, config.load_8bit, debug=config.debug)
 
         msg = message
 
-        conv = get_default_conv_template(model_path).copy()
+        conv = get_default_conv_template(config.model_path).copy()
         conv.append_message(conv.roles[0], msg)
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
@@ -47,7 +66,7 @@ def generate(messages):
             temperature=0.7,
             max_new_tokens=1024)
         outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
-        skip_echo_len = compute_skip_echo_len(model_path, conv, prompt)
+        skip_echo_len = compute_skip_echo_len(config.model_path, conv, prompt)
         outputs = outputs[skip_echo_len:]
 
         results.append(outputs)
