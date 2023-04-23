ca82677728f3bb0465cc3dbb46aea4bdcaefcb1d
cw75 <cgwu0530@gmail.com>
2023-04-23 04:53:19 +0000

    configurable param vicuna

--- aqueduct_llm/vicuna_7b.py
+++ aqueduct_llm/vicuna_7b.py
@@ -1,34 +1,42 @@
 import time
+from typing import List, Union
 
 import torch
 from fastchat.conversation import get_default_conv_template
 from fastchat.serve.inference import load_model, compute_skip_echo_len
 
 default_max_gpu_memory = '13GiB'
+default_temperature = 0.7
+default_max_new_tokens = 1024
 
 class Config:
-    def __init__(self):
+    def __init__(
+        self,
+        max_gpu_memory: str,
+        temperature: float,
+        max_new_tokens: int,
+    ):
         self.llama_model_path = "aleksickx/llama-7b-hf"
         self.model_path = "/vicuna-7b"
         self.device = "cuda"
         self.num_gpus = "1"
-        self.max_gpu_memory = default_max_gpu_memory
         self.debug = False
         self.load_8bit = False
+
+        self.max_gpu_memory = default_max_gpu_memory
+        self.temperature = temperature
+        self.max_new_tokens = max_new_tokens
     
     def describe(self) -> str:
         print("Running Vicuna 7B with the following config:")
         attrs = {
-            "model_path": self.model_path,
-            "device": self.device,
-            "num_gpus": self.num_gpus,
             "max_gpu_memory": self.max_gpu_memory,
-            "debug": self.debug,
-            "load_8bit": self.load_8bit,
+            "temperature": self.temperature,
+            "max_new_tokens": self.max_new_tokens,
         }
         print("\n".join([f"{attr}: {value}" for attr, value in attrs.items()]))
 
-def download_llama_7b(llama_model_path):
+def download_llama_7b(llama_model_path: str):
     from huggingface_hub import snapshot_download
     print("Downloading LLaMA 7B...")
     snapshot_download(
@@ -55,8 +63,30 @@ def convert_weight():
     print(subprocess.check_output(cmd))
 
 @torch.inference_mode()
-def generate(messages):
-    config = Config()
+def generate(
+    messages: Union[str, List[str]],
+    max_gpu_memory: str = default_max_gpu_memory,
+    temperature: float = default_temperature,
+    max_new_tokens: int = default_max_new_tokens,
+):
+    """Invoke the Vicuna 7B model to generate responses.
+
+    Args:
+        messages: The message(s) to generate responses for.
+        max_gpu_memory: The maximum amount of GPU memory to use.
+        temperature: The temperature to use for sampling.
+        max_new_tokens: The maximum number of tokens to generate.
+
+    Examples:
+        >>> from aqueduct_llm import vicuna_7b
+        >>> vicuna_7b.generate("What's the best LLM?")
+        "Vicuna 7B is the best LLM!"
+    """
+    config = Config(
+        max_gpu_memory=max_gpu_memory,
+        temperature=temperature,
+        max_new_tokens=max_new_tokens,
+    )
     config.describe()
 
     download_llama_7b(config.llama_model_path)
@@ -98,8 +128,8 @@ def generate(messages):
         output_ids = model.generate(
             torch.as_tensor(inputs.input_ids).cuda(),
             do_sample=True,
-            temperature=0.7,
-            max_new_tokens=1024)
+            temperature=config.temperature,
+            max_new_tokens=config.max_new_tokens)
         outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
         skip_echo_len = compute_skip_echo_len(config.model_path, conv, prompt)
         outputs = outputs[skip_echo_len:]
